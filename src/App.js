42f1012c54e51c857df9bbe1e7090675a33ed5cf
nareshbajariya <naresh.bajariya@saeculumsolutions.com>
2023-02-27 11:05:14 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit and save to reload.
-        </p>
-//new changes
+        <p>Edit and save to reload.</p>
+        //new changes
+        <p>Edit and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
